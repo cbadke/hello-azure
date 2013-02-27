adb3cf3548c9f1dd6867adb69fc00c50937da41c
Curtis Badke <curtis@badkid.ca>
2013-02-27 06:50:54 -0700

    Fresh F# MVC4 app from simplified template.

---
+++ hello-azureWebApp/Global.fs
@@ -0,0 +1,32 @@
+﻿namespace FsWeb
+
+open System
+open System.Web
+open System.Web.Mvc
+open System.Web.Routing
+open System.Web.Http
+open System.Data.Entity
+open System.Web.Optimization
+
+type BundleConfig() =
+    static member RegisterBundles (bundles:BundleCollection) =
+        bundles.Add(StyleBundle("~/Content/css").Include("~/Content/*.css"))
+
+type Route = { controller : string; action : string; id : UrlParameter }
+
+type Global() =
+    inherit System.Web.HttpApplication() 
+
+    static member RegisterGlobalFilters (filters:GlobalFilterCollection) =
+        filters.Add(new HandleErrorAttribute())
+
+    static member RegisterRoutes(routes:RouteCollection) =
+        routes.IgnoreRoute( "{resource}.axd/{*pathInfo}" )
+        routes.MapRoute("Default", "{controller}/{action}/{id}", 
+            { controller = "Home"; action = "Index"; id = UrlParameter.Optional } ) |> ignore
+
+    member this.Start() =
+        AreaRegistration.RegisterAllAreas()
+        Global.RegisterRoutes RouteTable.Routes
+        Global.RegisterGlobalFilters GlobalFilters.Filters
+        BundleConfig.RegisterBundles BundleTable.Bundles
